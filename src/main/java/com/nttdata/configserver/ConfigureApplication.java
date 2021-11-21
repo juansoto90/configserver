7c204d9aa4e3772a9bda320d7a8c6b48f9445f30
juansoto90 <jsotcast@nttdata.com>
2021-11-21 00:06:52 -0500

    refactor add bootstrap.yml

--- src/main/java/com/nttdata/configserver/ConfigureApplication.java
+++ src/main/java/com/nttdata/configserver/ConfigureApplication.java
@@ -2,12 +2,14 @@ package com.nttdata.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class ConfigserverApplication {
+public class ConfigureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+		SpringApplication.run(ConfigureApplication.class, args);
 	}
 
 }
